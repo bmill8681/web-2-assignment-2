3ad5f56e4588ad25862811c4bad67c8690b59475
Brett Miller <38405413+LLimb@users.noreply.github.com>
2019-11-20 14:10:37 -0700

    cities.php added

---
+++ cities.php
@@ -0,0 +1,19 @@
+<?php 
+    require_once 'config.php';
+    $sql = "SELECT CityCode, AsciiName, CountryCodeISO, Latitude, Longitude, Population, Elevation, TimeZone
+            FROM Cities
+            WHERE 1=1 ";
+    
+    if(isset($_GET['iso'])){
+        $sql .= "AND CountryCodeISO = $_GET['iso'] ";
+    }
+
+    $pdo = new PDO($DBSting, $user, $pass);
+    $queryResult = $pdo->query($sql);
+    $result = [];
+    while($row = $resultQuery->fetch()){
+        array_push($result, json_encode($row));
+    }
+    $pdo = null;
+    return $result;
+?>
