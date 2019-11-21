b641e8947680ffd34b380223be97d5e11dc75fec
nbesh761 <55159411+nbesh761@users.noreply.github.com>
2019-11-20 18:22:27 -0700

    Adding the countries.php Api file

---
+++ countries.php
@@ -0,0 +1,48 @@
+<?php 
+
+require_once('config.inc.php'); 
+      function formatRow($cur){
+        $data = [
+             "ISO"=>$cur[0],
+             "ISONumeric"=>$cur[1],
+             "CountryName"=>$cur[2],
+             "Capital"=>$cur[3],
+             "CityCode"=>$cur[4],
+             "Area"=>$cur[5],
+             "Population"=>$cur[6],
+             "Continent"=>$cur[7],
+             "TopLevelDomain"=>$cur[8],
+             "CurrencyCode"=>$cur[9],
+             "CurrencyCode"=>$cur[10],
+             "PhoneCountryCode"=>$cur[11],
+             "Languages"=>$cur[12],
+             "Neighbours"=>$cur[13],
+             "CountryDescription"=>$cur[14]
+        ];
+        return $data;
+    }
+
+  // Building the SQL query
+     $sql = "SELECT ISO, ISONumeric, CountryName, Capital, CityCode, Area, Population, Continent, TopLevelDomain, CurrencyCode, CurrencyName, PhoneCountryCode, Languages, Neighbours, CountryDescription ";
+
+     $sql .= "FROM Countries";
+
+     // Connecting to the DB
+    $pdo = new PDO(DBCONNSTRING, DBUSER, DBPASS);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $queryResult = $pdo->query($sql);
+    $result = array();
+
+    // Parsing the query results
+    while($row = $queryResult->fetch()){
+        $formatted = formatRow($row);
+        array_push($result, $formatted);
+    }
+    $pdo = null;
+
+    // Returning the value
+    echo json_encode($result);
+
+
+?>
+
