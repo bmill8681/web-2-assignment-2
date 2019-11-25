f462722af9ef4b1737e56cf1ad465071144a9a49
DCAvenue <45465461+DCAvenue@users.noreply.github.com>
2019-11-24 18:33:08 -0700

    fixed cities

--- cities.php
+++ cities.php
@@ -16,28 +16,35 @@
         return $data;
     }
 
-    // Building the SQL query
-    $sql = "SELECT CityCode, AsciiName, CountryCodeISO, Latitude, Longitude, Population, Elevation, TimeZone ";
-    $sql .= "FROM Cities WHERE 1=1 ";
-    
-    // Look at countries.php to see how to fix this using a prepared statement
-    if(isset($_GET['iso'])){
-        $sql .= "AND CountryCodeISO = ".$_GET['iso']." ";
-    }
-
     // Connecting to the DB
     $pdo = new PDO(DBCONNSTRING, DBUSER, DBPASS);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $queryResult = $pdo->query($sql);
+
+    // Building the SQL query and initial params
+    $sql = "SELECT CityCode, AsciiName, CountryCodeISO, Latitude, Longitude, Population, Elevation, TimeZone ";
+    $sql .= "FROM Cities WHERE 1=1 ";
+    $iso = null;
+    $queryResult = null;
     $result = array();
-
-    // Parsing the query results
-    while($row = $queryResult->fetch()){
-        $formatted = formatRow($row);
-        array_push($result, $formatted);
+    
+    // Build Query
+    if (isset($_GET['citycode'])) {
+        $sql .= "AND CityCode = :citycode";
+        $statement = $pdo->prepare($sql);
+        $statement->bindValue(":citycode", $_GET['citycode']);
+        $statement->execute();
+        $queryResult = $statement->fetchAll();
+        foreach($queryResult as $row){
+            array_push($result, formatRow($row));
+        }
     }
-    $pdo = null;
+    else {
+        $queryResult = $pdo->query($sql);
+        while ($row = $queryResult->fetch()) {
+            array_push($result, formatRow($row));
+        }
+    }
 
-    // Returning the value
-    echo json_encode($result);
-?>
+    // Return all
+    $pdo = null;
+    echo json_encode($result);    
