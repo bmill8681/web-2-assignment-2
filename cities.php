6d99bce7e1a88020381e5d1431bb75d1ba940e12
nbesh761 <55159411+nbesh761@users.noreply.github.com>
2019-11-24 19:48:14 -0700

    Updating the cities.php based using prepared statements

--- cities.php
+++ cities.php
@@ -16,26 +16,47 @@
         return $data;
     }
 
-    // Building the SQL query
-    $sql = "SELECT CityCode, AsciiName, CountryCodeISO, Latitude, Longitude, Population, Elevation, TimeZone ";
-    $sql .= "FROM Cities WHERE 1=1 ";
-    
-    // Look at countries.php to see how to fix this using a prepared statement
-    if(isset($_GET['iso'])){
-        $sql .= "AND CountryCodeISO = ".$_GET['iso']." ";
-    }
-
     // Connecting to the DB
     $pdo = new PDO(DBCONNSTRING, DBUSER, DBPASS);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $queryResult = $pdo->query($sql);
-    $result = array();
 
-    // Parsing the query results
-    while($row = $queryResult->fetch()){
-        $formatted = formatRow($row);
-        array_push($result, $formatted);
+
+
+    // Building the SQL query
+    $sql = "SELECT CityCode, AsciiName, CountryCodeISO, Latitude, Longitude, Population, Elevation, TimeZone ";
+    $sql .= "FROM Cities WHERE 1=1 ";
+
+    $iso = null;
+    $queryResult = null;
+    $result = array();
+    
+    // cities.php query using a prepared statement
+
+    if(isset($_GET['iso'])){
+        //$sql .= "AND CountryCodeISO = ".$_GET['iso']." ";
+        $sql .= "AND CountryCodeISO = :iso ";
+        $statement = $pdo->prepare($sql);
+        $statement->bindValue(":iso", $_GET['iso']);
+        $statement->execute();
+        $queryResult = $statement->fetchAll();
+        
+        foreach($queryResult as $row){
+        array_push($result, formatRow($row));
+        }
     }
+    else{
+         
+        $queryResult = $pdo->query($sql);
+        
+        //pasrsing the query results
+        while($row = $queryResult->fetch()){
+            $formatted = formatRow($row);
+            array_push($result, $formatted);
+        }
+        
+        
+    }
+
     $pdo = null;
 
     // Returning the value
