165083edcb7ce9702786b2bbd364924e99fe4ec3
nbesh761 <55159411+nbesh761@users.noreply.github.com>
2019-12-04 10:27:15 -0700

    Index.php page added

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <title>COMP 3512 Assign2</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="CSS/general.css">
+    <link rel="stylesheet" href="CSS/index.css">
+    <script src="JS/general.js"></script>
+    <script src="JS/index.js"></script>
+</head>
+
+<body>
+    <nav class="nav">
+        <div class="logo">LOGO</div>
+        <div class="navlinks">
+            <a href="home.php">Home</a>
+            <a href="about.php">About</a>
+            <a href="search.php">Browse</a>
+            <a href="single-country.php">Countries</a>
+            <a href="single-city.php">Cities</a>
+            <a href="upload.php">Upload</a>
+            <a href="profile.php">Profile</a>
+            <a href="favorites.php">Favorites</a>
+            <a href="login.php" class="active">Login</a>
+            <a href="signup.php">Signup</a>
+        </div>
+        <button class="hamburger">
+            <i class="fa fa-bars"></i>
+        </button>
+    </nav>
+
+    <main>
+        <div>
+            <section>
+                <button id="login">Login</button>
+                <button id="join">Join</button>
+                <input type="text" class="searchbox" placeholder="Search box for photos">
+            </section>
+        </div>
+    </main>
+
+    <footer>
+        <p class="copyright">© Group Assignment : Group Name : December 2019</p>
+
+
+
+    </footer>
+
+
+
+</body>
+
+</html>
