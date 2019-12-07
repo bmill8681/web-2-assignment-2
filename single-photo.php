196697f265e2320d91c757d10068a2680c38405b
Brett Miller <38405413+LLimb@users.noreply.github.com>
2019-12-07 11:35:46 -0700

    singlephoto 1/3rd done

--- single-photo.php
+++ single-photo.php
@@ -1,189 +1,112 @@
 <?php
-
 require_once('config.inc.php');
 require_once('helper.php');
 require_once('setConnection.php');
 
-
 function displayPic()
 {
-    if(isset($_GET["imageid"])){
-          
+    if (isset($_GET["imageid"])) {
+
         $imageId = $_GET["imageid"];
-//        echo "<div>" .$imageId . "</div>";
-         $picDetails = detailForSinglePhoto($imageId); 
-        
-        //print_r($picDetails);
-        foreach($picDetails as $p)
-        {   
+        $picDetails = detailForSinglePhoto($imageId);
+
+        foreach ($picDetails as $p) {
             $path = $p['Path'];
-            $path = strtolower( $path );
-            echo "<img class='picInfo' src='https://storage.googleapis.com/photosasg02/medium640/" . $path ."'>";
+            $path = strtolower($path);
+            echo "<img class='picInfo' src='https://storage.googleapis.com/photosasg02/medium640/" . $path . "'>";
         }
     }
 }
-
-
 function photoDetails()
 {
-    if(isset($_GET["imageid"])){
+    if (isset($_GET["imageid"])) {
         $imageId = $_GET["imageid"];
-        $picDetails = detailForSinglePhoto($imageId); 
-        
-        foreach($picDetails as $p)
-        {
+        $picDetails = detailForSinglePhoto($imageId);
+
+        foreach ($picDetails as $p) {
             $countryCode = $p['CountryCodeISO'];
             $Countryname = printNameOfCountry($countryCode);
-            
+
             $cityCode = $p['CityCode'];
             $cityname = printNameOfCities($cityCode);
-            
-            
-            
-            //call a function to get a country code and city code
-            echo "<h1>" . $p['Title']. "</h1>";
-            echo "<h2>  User ID: " . $p['UserID'] . "</h2>";
-            echo " <h2> <a href='countryView.php?iso=" . $p['CountryCodeISO'] . "'>" . $Countryname[0]['CountryName']. "</a></h2>,";
-            echo "<h2> <a href='countryView.php?iso=CA'>" . $cityname[0]['AsciiName'] . "</a></h2>";
-            
-        }
-    }
-    
-}
 
-function picDescription()
-{
-     if(isset($_GET["imageid"])){
-          
-        $imageId = $_GET["imageid"];
-        $picDetails = detailForSinglePhoto($imageId); 
-        //print_r($picDetails);
-        foreach($picDetails as $p)
-        {   
-            
-            echo "<div class='picDescription1'> <h4>". $p['Description'] ."</h4> </div>";
-            
-            
-            $data = json_decode($p['Exif']);
-            
-
-            
-            echo "<div class='picDetail'>" ;
-                echo "<h3> Make: " . $data->make . "</h3> "; 
-                echo "<h3> Model: " . $data->model . "</h3> "; 
-            echo "<h3> exposure_Time: " . $data->exposure_time . "</h3> "; 
-            echo "<h3> Aperture: " . $data->aperture . "</h3> "; 
-            echo "<h3> Focal Length: " . $data->focal_length . "</h3> "; 
-         
-                echo "</div>";
-
-            
-            echo "<picture class='picMap'> <img src='https://maps.googleapis.com/maps/api/staticmap?center=" . $p['Latitude'] . "," . $p['Longitude'] . "&zoom=12&size=500x500&maptype=hybrid&key=AIzaSyCMnhPvJhjCuI6n-8FIjVVPSJLzViJgfq4'></picture>";
-            
+            echo "<h1>" . $p['Title'] . "</h1>";
+            echo "<div class='titleDetails'>";
+            echo "<h2>User ID: " . $p['UserID'] . "</h2>";
+            echo "<a href='countryView.php?iso=".$p['CountryCodeISO']."'>";
+            echo "<h3>" . $cityname[0]['AsciiName'] . "</h3></a>";
+            echo "<a href='cityView.php?iso=" . $p['CityCode'] . "'>";
+            echo "<h3>" . $Countryname[0]['CountryName'] . "</h3></a>";
+            echo "</div>";
         }
     }
 }
-
-function displayDetailedInfo()
-{
-    if(isset($_GET["imageid"]))
-    {
-        $imageId = $_GET["imageid"];
-        $picDetails = detailForSinglePhoto($imageId); 
-        
-        foreach($picDetails as $p)
-        {
-             echo "<p>".$p['Description'] . "</p>";
-        }
-       
-        
-    }
-    
-}
-
-
 
 ?>
-
 
 <!DOCTYPE html>
 <html>
 
 <head>
-    <meta charset="UTF-8">
-
-    <link rel="stylesheet" href="CSS/general.css">
-     <link rel="stylesheet" href="CSS/singlePhoto.css">
-<!--    <link rel="stylesheet" href="CSS/country.css">-->
-  <script src="JS/general.js"></script>
-   <script src='JS/singlePhoto.js'></script>
-    <title>Single Photo View</title>
+    <meta charset="utf-8" />
+    <title>COMP 3512 Assign2</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+    <link rel="stylesheet" href="CSS/general.css" />
+    <link rel="stylesheet" href="CSS/singlePhoto.css" />
+    <script src="JS/general.js"></script>
+    <!-- <script src="JS/singlePhoto.js"></script> -->
 </head>
 
-<body >
-     <nav class="nav">
+<body>
+    <nav>
         <div class="logo">LOGO</div>
         <div class="navlinks">
             <a href="index.php">Home</a>
             <a href="about.php">About</a>
             <a href="search.php">Browse</a>
             <a href="countryView.php">Countries</a>
-            <a href="single-city.php">Cities</a>
-            <a href="upload.php">Upload</a>
-            <a href="profile.php">Profile</a>
-            <a href="favorites.php">Favorites</a>
+            <a href="cityView.php">Cities</a>
+            <!-- <a href="profile.php">Profile</a>
+            <a href="favorites.php">Favorites</a> -->
             <a href="login.php" class="active">Login</a>
             <a href="signup.php">Signup</a>
+            <?php
+            if (isset($_SESSION['id'])) {
+                echo "<a href='logout.php'>Log Out</a>";
+            } else {
+                echo "<a href='profile.php'>Profile</a>";
+                echo "<a href='favorites.php'>Favorites</a>";
+            }
+
+            ?>
         </div>
         <button class="hamburger">
             <i class="fa fa-bars"></i>
         </button>
     </nav>
-     
-    <main >
-        <section id="left">
-            <div> <?php displayPic() ?></div>
-                <section class="box">
-                     <?php displayDetailedInfo(); ?>            
-                </section>
-        </section>
-        <section id="right" >
-            <header>
-                <section id="photoDetails">
-                   <?php photoDetails();  ?>
-<!--
-                    <h1>Photo Title</h1>
-                    <h2>User Name</h2>
-                    <h2>Country, City</h2>
--->
-                </section>
-                <section id="photoControls">
-                    <button>Add To Favorites</button>
-                </section>
-            </header>
-            <section id="tabContainer">
-                <header>
-                    <div class="tab tab1">Description</div>
-                    
-                    <div class="tab details">Details</div>
-                    
-                    <div class="tab tab3">Map
-                       
-                    </div>
-                    
-                </header>
-                <section id="info">
-                    <?php picDescription(); ?>
-                </section>
+
+    <main>
+        <div>
+            <section class="photoWrapper">
+                <?php displayPic(); ?>
             </section>
-        </section>
+            <section class="titleWrapper">
+                <section>
+                    <?php photoDetails();  ?>
+                </section>
+                <button>Add To Favorites</button>
+            </section>
+            <section class="detailWrapper">
+                Details
+            </section>
+        </div>
     </main>
-     <footer>
+
+    <footer>
         <p class="copyright">© Group Assignment : Group Name : December 2019</p>
-
-
-
     </footer>
+
 </body>
 
 </html>
